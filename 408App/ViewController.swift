87d254ef809c3ba0207a4450fc2987d7d100abe1
thefreshduke <scottyshaw1@gmail.com>
2015-08-31 09:24:20 +0100

    Mysteries are no fun

--- 408App/ViewController.swift
+++ 408App/ViewController.swift
@@ -6,17 +6,10 @@
 //  Copyright (c) 2015 ___sks6___. All rights reserved.
 //
 
-<<<<<<< HEAD
 import Foundation
 import UIKit
 
 class ViewController: UIViewController {
-=======
-import UIKit
-
-class ViewController: UIViewController {
-    
->>>>>>> 3db075c574a561594d51b1f7d9181a4f8e582a30
     @IBOutlet var connectionCheckLabel: UILabel!
     
     override func viewDidLoad() {
--- LogInViewController.swift
+++ LogInViewController.swift
@@ -10,29 +10,16 @@ import Foundation
 import UIKit
 
 class logInViewController : UIViewController {
-<<<<<<< HEAD
-=======
-    
->>>>>>> 3db075c574a561594d51b1f7d9181a4f8e582a30
     @IBOutlet var LogInLabel: UILabel!
     @IBOutlet var logInUsername: UITextField!
     @IBOutlet var logInPassword: UITextField!
     @IBOutlet var logInSavePasswordLabel: UILabel!
     @IBOutlet var logInsavePasswordSwitch: UISwitch!
     
-<<<<<<< HEAD
     // Currently allows login as long as neither text field is empty
     @IBAction func logInActionButton(sender: AnyObject) {
         if logInUsername.text != "" && logInPassword.text != "" {
             self.performSegueWithIdentifier("LogInSegue", sender: self)
-=======
-    @IBAction func logInActionButton(sender: AnyObject) {
-        
-        if logInUsername.text != "" && logInPassword.text != "" {
-            if logInsavePasswordSwitch.on {
-                self.performSegueWithIdentifier("LogInSegue", sender: self)
-            }
->>>>>>> 3db075c574a561594d51b1f7d9181a4f8e582a30
         }
     }
 }
